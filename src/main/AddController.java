96c037d21038f4f69883d2272ad9617b7bf68c47
Chaitanya <chaitu.vsh@gmail.com>
2020-11-08 12:08:31 +0530

    SpringMVC: Implemented LOGGER for both the controllers

--- src/main/AddController.java
+++ src/main/AddController.java
@@ -1,19 +1,23 @@
 package main;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.logging.Logger;
 
 
 @Controller
 public class AddController {
 
+    private final static Logger LOGGER = Logger.getLogger(AddController.class.getName());
+
     @RequestMapping("/add")
     public ModelAndView add(HttpServletRequest request, HttpServletResponse response){
+
+        LOGGER.info("Performing the Addition");
         int i = Integer.parseInt(request.getParameter("t1"));
         int j = Integer.parseInt(request.getParameter("t2"));
 
@@ -23,12 +27,13 @@ public class AddController {
         mv.setViewName("display.jsp");
         mv.addObject("result",result);
 
-
         return mv;
     }
 
     @RequestMapping("/subtract")
     public ModelAndView subtract(HttpServletRequest request, HttpServletResponse response){
+
+        LOGGER.info("Performing the Subtraction");
         int i = Integer.parseInt(request.getParameter("s1"));
         int j = Integer.parseInt(request.getParameter("s2"));
 
