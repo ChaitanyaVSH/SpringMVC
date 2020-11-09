e9a9eb3eef2cda239bd50baa601689abd037634e
Chaitanya <chaitu.vsh@gmail.com>
2020-11-10 00:12:48 +0530

    SpringMVC: Adding Meta Annotations over the custom annotations

--- src/main/annotations/MathematicsRole.java
+++ src/main/annotations/MathematicsRole.java
@@ -1,5 +1,13 @@
 package main.annotations;
 
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MathematicsRole {
 
     String role() default "Arithmetic";
